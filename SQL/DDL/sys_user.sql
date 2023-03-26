e4c91b3aa33c1f9a5f9b830ff08302e516b82758
MoonerHigh <Maozilox@Gmail.com>
2023-03-26 23:09:44 +0800

    build：项目结构调整

---
+++ SQL/DDL/sys_user.sql
@@ -0,0 +1,22 @@
+-- Taste.sys_user definition
+
+CREATE TABLE `sys_user` (
+                            `id` bigint NOT NULL COMMENT 'id',
+                            `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
+                            `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '密码',
+                            `real_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '姓名',
+                            `head_url` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '头像',
+                            `gender` tinyint unsigned DEFAULT NULL COMMENT '性别   0：男   1：女    2：保密',
+                            `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '邮箱',
+                            `mobile` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '手机号',
+                            `dept_id` bigint DEFAULT NULL COMMENT '部门ID',
+                            `super_admin` tinyint unsigned DEFAULT NULL COMMENT '超级管理员   0：否   1：是',
+                            `status` tinyint DEFAULT NULL COMMENT '状态  0：停用   1：正常',
+                            `creator` bigint DEFAULT NULL COMMENT '创建者',
+                            `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+                            `updater` bigint DEFAULT NULL COMMENT '更新者',
+                            `update_date` datetime DEFAULT NULL COMMENT '更新时间',
+                            PRIMARY KEY (`id`) USING BTREE,
+                            UNIQUE KEY `uk_username` (`username`) USING BTREE,
+                            KEY `idx_create_date` (`create_date`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='系统用户';
