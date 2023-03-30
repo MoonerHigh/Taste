21d13c58e51c44f64464166ce55c9002be6712a0
zhangpanlong <Maozilox@Gmail.com>
2023-03-30 14:32:48 +0800

    build:Admin模块构建

---
+++ taste-admin/src/main/java/com/moonerhigh/tasteadmin/TasteAdminApplication.java
@@ -0,0 +1,13 @@
+package com.moonerhigh.tasteadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasteAdminApplication.class, args);
+    }
+
+}
