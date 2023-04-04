adcea0b4c22b5c8345a541896485b1b75ea5d6d5
zhangpanlong <Maozilox@Gmail.com>
2023-04-04 18:28:19 +0800

    build:管理后台模块重构

---
+++ taste-server/src/main/java/com/moonerghig/taste/admin/TasteServerApplication.java
@@ -0,0 +1,19 @@
+package com.moonerghig.taste.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author zpLone
+ * @Date 2023/4/4 18:23
+ * @PackageName:com.moonerghig.taste.admin
+ * @ClassName: TasteServerApplication
+ * @Description: 管理后台启动类
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class TasteServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TasteServerApplication.class);
+    }
+}
