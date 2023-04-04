b88d4d23cdc4124d7948a22cb188f968c01c4958
zhangpanlong <Maozilox@Gmail.com>
2023-04-04 17:36:31 +0800

    build:管理后台模块重构

--- taste-server/src/main/java/com/moonerhigh/taste/server/TasteServerApplication.java
+++ taste-server/src/main/java/com/moonerhigh/taste/server/TasteServerApplication.java
@@ -1,13 +1,13 @@
-package com.moonerhigh.tasteadmin;
+package com.moonerhigh.taste.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TasteAdminApplication {
+public class TasteServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TasteAdminApplication.class, args);
+        SpringApplication.run(TasteServerApplication.class, args);
     }
 
 }
