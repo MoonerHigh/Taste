e5db3f6ea352acb53a0cbf07fc3aac742c4630a9
zhangpanlong <Maozilox@Gmail.com>
2023-04-20 18:17:37 +0800

    feat: 计算日期间隔天数工具类

--- framework/framework-common/src/main/java/com/moonerhigh/framework/common/utils/DateUtil.java
+++ framework/framework-common/src/main/java/com/moonerhigh/framework/common/utils/DateUtil.java
@@ -2,6 +2,11 @@ package com.moonerhigh.framework.common.utils;
 
 import org.apache.commons.lang3.time.DateUtils;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
 /**
  * @Author zpLone
  * @Date 2023/4/12 12:00
@@ -13,5 +18,15 @@ import org.apache.commons.lang3.time.DateUtils;
 public class DateUtil extends DateUtils {
     public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
 
-
+    public static Long dateDiff(Date startTime, Date endTime) {
+        long between;
+        if (startTime != null && endTime != null) {
+            LocalDateTime startLocalDateTime = startTime.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            LocalDateTime endLocalDateTime = endTime.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            between = ChronoUnit.DAYS.between(startLocalDateTime, endLocalDateTime);
+        } else {
+            between = 0L;
+        }
+        return between;
+    }
 }
