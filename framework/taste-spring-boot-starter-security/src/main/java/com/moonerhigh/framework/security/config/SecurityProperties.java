4d637b259bfeda8a01afa09edb8fd077e861ebe1
zhangpanlong <Maozilox@Gmail.com>
2023-05-19 11:34:14 +0800

    build:framework-security

---
+++ framework/taste-spring-boot-starter-security/src/main/java/com/moonerhigh/framework/security/config/SecurityProperties.java
@@ -0,0 +1,15 @@
+package com.moonerhigh.framework.security.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @Date 2023/5/18 19:04
+ * @PackageName:com.moonerhigh.framework.security.config
+ * @ClassName: SecurityProperties
+ * @Description: SpringSecurity配置文件实体类
+ * @Version 1.0
+ */
+@ConfigurationProperties
+public class SecurityProperties {
+
+}
